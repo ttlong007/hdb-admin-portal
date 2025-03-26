0f0d567d07037560174e5e2e7e7929ec0fac7d00
Thuong To <thuongthanhto@gmail.com>
2025-03-26 08:40:26 +0700

    Remove color dark

--- src/layouts/hydrogen/layout.tsx
+++ src/layouts/hydrogen/layout.tsx
@@ -8,7 +8,7 @@ export default function HydrogenLayout({
 }) {
   return (
     <main className="flex min-h-screen flex-grow">
-      <Sidebar className="fixed hidden dark:bg-gray-50 xl:block" />
+      <Sidebar className="fixed hidden xl:block" />
       <div className="flex w-full flex-col xl:ms-[270px] xl:w-[calc(100%-270px)] 2xl:ms-72 2xl:w-[calc(100%-288px)]">
         <Header />
         <div className="flex flex-grow flex-col px-4 pb-6 pt-2 md:px-5 lg:px-6 lg:pb-8 3xl:px-8 3xl:pt-4 4xl:px-10 4xl:pb-9 bg-[#FAF7F6]">
