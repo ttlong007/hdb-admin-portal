247781cae69a446dfb73a09f5a297110eeca6f43
Thuong To <thuongthanhto@gmail.com>
2025-03-26 08:38:32 +0700

    Add filter

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { BsDownload } from 'react-icons/bs'
+import { Input, Select } from 'rizzui'
 
 const Home: React.FC = () => {
   return (
@@ -45,7 +47,32 @@ const Home: React.FC = () => {
         </div>
       </div>
 
-      <div className="self-stretch p-6 bg-[#F8FAFC] rounded-sm outline outline-1 outline-[#DAE0E7] inline-flex flex-col justify-start items-start gap-4"></div>
+      <div className="self-stretch p-6 bg-[#F8FAFC] rounded-sm outline outline-1 outline-[#DAE0E7] inline-flex flex-col justify-start items-start gap-4">
+        <div className="grid grid-cols-5 gap-4 w-full">
+          <Input label="Mã CIF" placeholder="Mã CIF" />
+          <Input label="Tên công ty" placeholder="Tên công ty" />
+          <Input label="Số cửa hàng đại lý" placeholder="Số cửa hàng đại lý" />
+          <Input label="Tên đại lý" placeholder="Tên đại lý" />
+          <Select
+            options={[]}
+            value={null}
+            onChange={() => {}}
+            label="Trạng thái"
+            dropdownClassName="h-auto"
+          />
+        </div>
+        <div className="flex justify-end gap-4 w-full">
+          <button className="bg-white rounded-sm outline outline-1 outline-offset-[-1px] outline-sky-900/20 inline-flex justify-center items-center gap-2 px-4 py-2 text-black/60 text-base font-semibold">
+            <BsDownload /> Tải xuống
+          </button>
+          <button className="bg-white rounded-sm outline outline-1 outline-offset-[-1px] outline-sky-900/20 inline-flex justify-center items-center gap-2 px-4 py-2 text-black/60 text-base font-semibold">
+            <BsDownload /> Làm mới
+          </button>
+          <button className="rounded-sm outline outline-1 outline-offset-[-1px] outline-sky-900/20 inline-flex justify-center items-center gap-2 px-4 py-2 bg-[#DA2128] text-base font-semibold text-white">
+            Áp dụng
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
