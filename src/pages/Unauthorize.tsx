44f81c46488575e0cc945796e7e7351728d81ec2
Thuong To <thuongthanhto@gmail.com>
2025-05-30 09:22:47 +0700

    chore: build and deploy to uat

--- src/pages/Unauthorize.tsx
+++ src/pages/Unauthorize.tsx
@@ -1,30 +1,8 @@
 import React from 'react'
 
 import Atom from '@/components/core/components/AtomLoading'
-import { getEnv } from '@/config/env'
-import { useAuth } from '@/store/authSlice/useAuth'
-import { routes } from '@/config/routes'
-import { useNavigate } from 'react-router-dom'
 
 const Unauthorize: React.FC = () => {
-  const { setAuthState } = useAuth()
-  const navigate = useNavigate()
-  const handleLogin = () => {
-    localStorage.removeItem('accessToken')
-    localStorage.removeItem('refreshToken')
-    setAuthState({
-      objectKeyStaff: null,
-      objectKeyMerchant: null,
-    })
-    const ROOT_CALLBACK_URL = getEnv(
-      'VITE_ROOT_CALLBACK_URL',
-      'https://ungdung.hdbank.com.vn/Login'
-    )
-    window.location.replace(
-      ROOT_CALLBACK_URL || 'https://ungdung.hdbank.com.vn/Login'
-    )
-  }
-
   return (
     <div className="h-[90%] flex flex-col items-center justify-center">
       <div className="mt-10 flex flex-col items-center">
