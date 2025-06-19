46a68bfddd4a78fbcbc360b0065561905d081369
Thuong To <thuongthanhto@gmail.com>
2025-06-19 16:09:53 +0700

    chore: build and deploy to uat

--- src/pages/Merchants/CreateMerchant.tsx
+++ src/pages/Merchants/CreateMerchant.tsx
@@ -36,7 +36,7 @@ const CreateMerchant = () => {
     queryKey: ['transaction-types'],
     queryFn: async () => {
       const response = await axiosInstance.get(
-        '/v1/admin/transaction/list-types?is_financial=true'
+        '/v1/admin/transaction/list-types?need_approval=true'
       )
       if (response.data.status_code === 'ACCEPT') {
         return response.data.data
